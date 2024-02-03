66e9494d6e13ffa2295b548a22e9942a961f7a42
Chandler Davis <chandler.davis99@icloud.com>
2024-02-03 15:47:59 -0500

    more work

---
+++ social/public/grpc_social_v1/players.go
@@ -0,0 +1,15 @@
+package grpc_social_v1
+
+import (
+	"context"
+
+	"github.com/bitcrshr/pwm/protos/compiled/go/social_v1"
+)
+
+func (s *server) GetOnlinePlayers(ctx context.Context, req *social_v1.GetOnlinePlayersRequest) (*social_v1.GetOnlinePlayersResponse, error) {
+	return nil, nil
+}
+
+func (s *server) GetServerPlayers(ctx context.Context, req *social_v1.GetServerPlayersRequest) (*social_v1.GetServerPlayersResponse, error) {
+	return nil, nil
+}
--- social/public/grpc_social_v1/server.go
+++ social/public/grpc_social_v1/server.go
@@ -1,1 +1,30 @@
 package grpc_social_v1
+
+import (
+	"net"
+
+	"github.com/bitcrshr/pwm/protos/compiled/go/social_v1"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	social_v1.UnimplementedSocialServiceServer
+}
+
+func Serve() error {
+	lis, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		return err
+	}
+
+	grpcServer := grpc.NewServer()
+	s := server{}
+
+	social_v1.RegisterSocialServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		return err
+	}
+
+	return nil
+}
